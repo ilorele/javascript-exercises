45f50d77279b089e30d34ad44aca017ca9fdc272
Magdalena Rozpara <magda.rozpara96@gmail.com>
2023-10-16 19:38:39 +0200

    Complete task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    if (index < 0) {
+        return "OOPS";
+    }
 
+    const fibonacciArr = [0, 1];
+
+    for (i = 1; i < index; i++) {
+        fn = fibonacciArr[fibonacciArr.length - 2] + fibonacciArr[fibonacciArr.length -1];
+        // console.log("fn: " + fn);
+        fibonacciArr.push(fn);
+    }
+
+    return fibonacciArr[fibonacciArr.length -1];
 };
 
 // Do not edit below this line
