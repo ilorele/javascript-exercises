87499e94fedef520bc7a5ce99b6ebff42e7fd033
Magdalena Rozpara <magda.rozpara96@gmail.com>
2023-10-17 18:37:30 +0200

    Complete task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArr) {
+    const currentYear = (new Date).getFullYear();
+    
+    peopleArr.forEach(person => {
+        if (!("yearOfDeath" in person)) {
+            person.yearOfDeath = currentYear;
+        }
+    });
 
+    peopleArr.sort((a, b) => {
+        a = a.yearOfDeath - a.yearOfBirth;
+        b = b.yearOfDeath - b.yearOfBirth;
+        return a < b ? 1 : -1;
+    });
+
+    console.log(peopleArr);
+    return peopleArr[0];
 };
 
 // Do not edit below this line
